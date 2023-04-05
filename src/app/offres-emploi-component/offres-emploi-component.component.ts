93513330f049308f324d53b6e0c75ea600b49edb
unknown <jihed.mrouki@gmail.com>
2023-04-05 03:03:51 +0100

    follwo up part one

--- src/app/offres-emploi-component/offres-emploi-component.component.ts
+++ src/app/offres-emploi-component/offres-emploi-component.component.ts
@@ -6,16 +6,21 @@ import { Emploi } from '../model/emploi';
   styleUrls: ['./offres-emploi-component.component.css']
 })
 export class OffresEmploiComponentComponent implements OnInit {
-listEmploi:Emploi[]=[
+
+  listEmploi:Emploi[]=[
  {reference: "AAAA" , title:"emploi 1", entreprise:"VDS1",etat:true},
  {reference: "BBBB" , title:"emploi 2", entreprise:"VDS2",etat:false},
  {reference: "CCCC" , title:"emploi 2", entreprise:"VDS3",etat:false},
  {reference: "DDD" , title:"emploi 4", entreprise:"VDS4",etat:true}
 
 ]
+inc=0;
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  availableEmploi(inc:number){
+    inc=inc++;
+    }
 }
